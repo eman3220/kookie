83362be928f14d3794484f70fb9186e8ada90ed3
eman3220 <manuel.godinez22@gmail.com>
2020-01-08 18:18:45 +1300

    Updates

---
+++ precache-manifest.5f3654884c4513b2fc2d81323c0e2267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c3489381f4169dca61180dcb078a18",
+    "url": "/kookie/index.html"
+  },
+  {
+    "revision": "08d0b001a257b1bdff12",
+    "url": "/kookie/static/css/main.e5c4b854.chunk.css"
+  },
+  {
+    "revision": "6f3ea4527b9d2aecaa6e",
+    "url": "/kookie/static/js/2.3660cb91.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/kookie/static/js/2.3660cb91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08d0b001a257b1bdff12",
+    "url": "/kookie/static/js/main.9e3994f8.chunk.js"
+  },
+  {
+    "revision": "4bf35708c9218fcf6cca",
+    "url": "/kookie/static/js/runtime-main.684f3448.js"
+  }
+]);
